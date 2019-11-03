75193d90e17f0a3e0771548348d18ccb29e33cc5
mbolters <mariematisans@gmail.com>
2019-11-02 22:08:24 -0500

    typo

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,7 @@
 
 // * Also inside `burger.js`, create the code that will call the ORM functions using burger specific input for the ORM.
 
-var orm = require('./../../../config/orm.js');
+var orm = require('./../../../config/orm');
 
 var burger = {
   all: function(cb) {
