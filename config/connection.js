9c738791fe71a6c8844765ae40a9203fec7fcfb4
mbolters <mariematisans@gmail.com>
2019-11-02 23:25:09 -0500

    create database connection

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,20 @@ var mysql = require('mysql');
 const myConfig = require('../config');
 
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: myConfig.config.password,
-  database: "burger_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+	// DB is JawsDB on Heroku
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: myConfig.config.password,
+    database: "burger_db"
+  });
+};
 
 connection.connect(function(err) {
   if (err) {
