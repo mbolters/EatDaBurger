c21ebad63ece7f3c97549f243cd3190e1490ecd8
mbolters <mariematisans@gmail.com>
2019-11-03 20:52:45 -0600

    update id

--- db/burger.sql
+++ db/burger.sql
@@ -4,11 +4,9 @@ USE burger_db;
 
 CREATE TABLE burgers
 (
-	id  INTEGER AUTO_INCREMENT,
+	id INTEGER AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(255) NOT NULL,
-    eaten BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
-
+    eaten BOOLEAN DEFAULT false
 ); 
 
 SET PASSWORD FOR root@localhost=PASSWORD('');
